bae4abff95b813c5a321e102f9be74dbea153c46
geovanedesouza <geovaneunipar@gmail.com>
2014-05-12 23:10:17 -0300

    Tela 3

---
+++ docs/Tela3.php
@@ -0,0 +1,57 @@
+<html>
+    <head>
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen" type="text/css">
+		<style type="text/css">
+	#form {  padding:15px;}
+	div.box .control-label { padding:10px 0 10px 0;}
+	</style>
+        <title></title>
+		
+	
+    </head>
+	
+    <body>
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    
+		
+  <form class="form-inline" id="form" method="post">
+<fieldset>
+
+<div class="box">
+<legend>3. Pre&ccedilos</legend>
+
+<label class="control-label" for="textinput">3.1 Como forma os pre&ccedilos de seus produtos?</label>
+	<div>
+      <input type="radio" name="radios1"id="311" value="Sim">
+      Percentual sobre os custos
+      <input type="radio" name="radios1" id="312" value="N&atildeo">
+      Acompanhando o mercado
+	  <input type="radio" name="radios1" id="313" value="N&atildeo">
+      Outra maneira, qual?
+</div>
+
+<hr></hr>
+
+<label class="control-label" for="textinput">3.2 Seus pre&ccedilos em rela&ccedil&atildeo a concorr&ecircncia est&atildeo?</label>
+	<div>
+      <input type="radio" name="radios2" id="321" value="Sim">
+      Acima
+      <input type="radio" name="radios2" id="322" value="N&atildeo">
+      Iguais
+	  <input type="radio" name="radios2" id="323" value="N&atildeo">
+      Abaixo
+</div>
+<hr></hr>
+
+  <div class="controls">
+    <button id="salvar" name="salvar" class="btn btn-success">Salvar</button>
+  </div>
+</div>
+</fieldset>
+</form>
+
+
+       
+    </body>
+</html>
