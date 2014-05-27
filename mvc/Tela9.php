78410a9f170a74509592a9e4e30092848512ffd9
geovanedesouza <geovaneunipar@gmail.com>
2014-05-26 23:28:17 -0300

    Create Tela9.php

---
+++ mvc/Tela9.php
@@ -0,0 +1,101 @@
+<html>
+    <head>
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen" type="text/css">
+	<style type="text/css">
+	#form {  padding:15px;}
+	div.box .control-label { padding:10px 0 10px 0;}
+	</style>
+	
+        <title></title>
+	
+    </head>
+	
+    <body>
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    
+	<div class="box">	
+  <form class="form-inline" id="form" method="POST">
+<fieldset>
+
+
+<legend>9. Jur&iacutedico</legend>
+
+<label class="control-label" for="textinput">9.1 Voc&ecirc conhece todos os impostos devidos pela sua empresa?</label>
+	<div>
+      <input type="radio" name="radios1" id="91" value="sim">
+      Sim
+      <input type="radio" name="radios1" id="91" value="nao">
+      N&atildeo
+  </div>
+  
+  <hr></hr>
+  
+  <label class="control-label" for="textinput">9.2 Voc&ecirc j&aacute recebeu alguma autua&ccedil&atildeo de fiscaliza&ccedil&atildeo?</label>
+	<div>
+      <input type="radio" name="radios2" id="92" value="sim">
+      Sim
+      <input type="radio" name="radios2" id="92" value="nao">
+      N&atildeo
+  </div>
+  
+   <hr></hr>
+  
+  <label class="control-label" for="textinput">9.3 Sua empresa est&aacute respondendo alguma a&ccedil&atildeo judicial, inclusive trabalhista?</label>
+	<div>
+      <input type="radio" name="radios3" id="93" value="sim">
+      Sim
+      <input type="radio" name="radios3" id="93" value="nao">
+      N&atildeo
+  </div>
+
+   <hr></hr>
+  
+  <label class="control-label" for="textinput">9.4 Suas tratativas com seus fornecedores s&atildeo:</label>
+	<div>
+      <input type="radio" name="radios41" id="94" value="verbais">
+      Verbais
+      <input type="radio" name="radios42" id="94" value="contrato">
+      Com contratos
+  </div>
+  
+   <hr></hr>
+  
+  <label class="control-label" for="textinput">9.5 Como voc&ecirc realiza suas cobran&ccedilas na empresa?</label>
+	<div>
+      <input type="radio" name="radios51" id="95" value="verbais">
+      Empresa de cobran&ccedila
+      <input type="radio" name="radios52" id="95" value="contrato">
+      Colaborador
+	  <input type="radio" name="radios53" id="95" value="contrato">
+      Pessoalmente
+	  <input type="radio" name="radios54" id="95" value="contrato">
+      Outros
+  </div>
+  
+  <hr></hr>
+  
+  <label class="control-label" for="textinput">9.6 Tem assessoria jur&iacutedica?</label>
+	<div>
+      <input type="radio" name="radios6" id="96" value="sim">
+      Sim
+      <input type="radio" name="radios6" id="96" value="nao">
+      N&atildeo
+  </div>
+  
+  <hr></hr>
+  <label class="control-label" for="textinput">Observa&ccedil&atildeo</label>  
+    <input id="obs" name="obs" type="text" class="input-xlarge"> 
+<hr></hr>
+<label class="control-label" for="salvar"></label>
+  <div class="controls">
+    <button id="salvar" name="salvar" class="btn btn-success">Salvar</button>
+  </div>
+</div>
+</fieldset>
+</form>
+
+
+       
+    </body>
+</html>
