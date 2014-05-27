33472db9a3f5d15cb8118b80fa7ce02fce9cf5e9
geovanedesouza <geovaneunipar@gmail.com>
2014-05-26 23:27:08 -0300

    Create Tela7.php

---
+++ mvc/Tela7.php
@@ -0,0 +1,96 @@
+<html>
+    <head>
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen" type="text/css">
+		<style type="text/css">
+	#form {  padding:15px;}
+	div.box .control-label { padding:10px 0 10px 0;}
+	</style>
+        <title></title>
+		
+	
+    </head>
+	
+    <body>
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    
+		
+  <form class="form-inline" id="form" method="post">
+<fieldset>
+
+<div class="box">
+<legend>7. Recursos Humanos</legend>
+ <label class="control-label" for="textinput">7.1 Quantos colaboradores possui?</label>  
+    <input id="colaborador" name="colaborador" type="text" class="input-xlarge"> 
+
+<hr></hr>
+<label class="control-label" for="textinput">7.2 No &uacuteltimo ano seus colaboradores participaram de treinamento?</label>
+	<div>
+      <input type="radio" name="radios1" id="72" value="sim">
+      Sim. Em que &aacuterea? <input id="area7" name="area7" type="text" class="input-xlarge"> <br>
+      <input type="radio" name="radios1" id="72" value="nao">
+      N&atildeo
+  </div>
+
+<hr></hr>
+<div>
+<label class="control-label" for="textinput">7.3 Qual a escolaridade dos que trabalham na empresa?</label>
+	<div>
+      <input type="radio" name="radios31" id="731" value="segundo">
+      Segundo grau
+      <input type="radio" name="radios32" id="732" value="superior">
+      Superior
+	  <input type="radio" name="radios33" id="733" value="pos">
+      P&oacutes-gradua&ccedil&atildeo
+  </div>
+</div>
+  <hr></hr>
+ <label class="control-label" for="textinput">7.4 Que crit&eacuterios utiliza para contrata&ccedil&atildeo de colaboradores?</label>
+	<div>
+      <input type="radio" name="radios41" id="741" value="indicacao">
+      Indica&ccedil&atildeo
+      <input type="radio" name="radios42" id="742" value="entrevista">
+      Entrevista
+	  <input type="radio" name="radios43" id="743" value="analise">
+      An&aacutelise de curr&iacuteculo e entrevista
+  </div>
+ <hr></hr>
+ <label class="control-label" for="textinput">7.5 Quais s&atildeo os fatores priorit&aacuterios na escolha de um candidato? Enumerar por ordem de import&acircncia.</label>
+	<div>
+      <input type="radio" name="radios51" id="751" value="indicacao">
+      Indica&ccedil&atildeo
+      <input type="radio" name="radios52" id="752" value="entrevista">
+      Entrevista
+	  <input type="radio" name="radios22" id="753" value="analise">
+      An&aacutelise de curr&iacuteculo e entrevista
+  </div>
+
+<hr></hr>
+ <label class="control-label" for="textinput">7.6 Que tipo de atividades realiza para manter seus funcion&aacuterios motivados?</label>
+	<div>
+      <input type="radio" name="radios21" id="751" value="festas">
+      Festas especiais<br>
+      <input type="radio" name="radios22" id="752" value="participacao">
+      Participa&ccedil&atildeo no lucro<br>
+	  <input type="radio" name="radios22" id="753" value="outros">
+      Outros:  <input id="outros7" name="outros" type="text" class="input-xlarge">
+  </div>
+<hr></hr>
+
+<div>
+<label class="control-label" for="textinput">7.7 Quantas demiss&otildees X Contrata&ccedil&otildees por ano? Por qu&ecirc?</label>  
+    <input id="77" name="77" type="text" class="input-xlarge"> 
+<hr></hr>
+</div>
+  <div class="controls">
+    <button id="salvar" name="salvar" class="btn btn-success">Salvar</button>
+  </div>
+
+</div>
+</fieldset>
+</form>
+
+
+       
+    </body>
+</html>
